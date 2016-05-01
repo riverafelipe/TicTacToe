b19f9fe142fc343a5e929222604b8060e52d19aa
Felipe Rivera <FelipeRivera@franciscos-macbook.local>
2016-04-30 20:44:36 -0400

    Integrated all the possible combinations and the console prints out the winner.

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -8,9 +8,45 @@
 
 import UIKit
 
+
+var xPlays: [Int] = []
+
+var oPlays: [Int] = []
+
+
+func winner() {
+    
+    if (xPlays.contains(1) && xPlays.contains(2) && xPlays.contains(3)) ||
+        (xPlays.contains(4) && xPlays.contains(5) && xPlays.contains(6)) ||
+        (xPlays.contains(7) && xPlays.contains(8) && xPlays.contains(9)) ||
+        (xPlays.contains(1) && xPlays.contains(4) && xPlays.contains(7)) ||
+        (xPlays.contains(2) && xPlays.contains(5) && xPlays.contains(8)) ||
+        (xPlays.contains(3) && xPlays.contains(6) && xPlays.contains(9)) ||
+        (xPlays.contains(1) && xPlays.contains(5) && xPlays.contains(9)) ||
+        (xPlays.contains(3) && xPlays.contains(5) && xPlays.contains(7)) {
+        
+        print("Player 1 Wins!")
+        
+    } else if (oPlays.contains(1) && oPlays.contains(2) && oPlays.contains(3)) ||
+    (oPlays.contains(4) && oPlays.contains(5) && oPlays.contains(6)) ||
+    (oPlays.contains(7) && oPlays.contains(8) && oPlays.contains(9)) ||
+    (oPlays.contains(1) && oPlays.contains(4) && oPlays.contains(7)) ||
+    (oPlays.contains(2) && oPlays.contains(5) && oPlays.contains(8)) ||
+    (oPlays.contains(3) && oPlays.contains(6) && oPlays.contains(9)) ||
+    (oPlays.contains(1) && oPlays.contains(5) && oPlays.contains(9)) ||
+    (oPlays.contains(3) && oPlays.contains(5) && oPlays.contains(7)) {
+    
+    print("Player 2 Wins!")
+    
+    }
+}
+
+
 class ViewController: UIViewController {
   
     var tapCount = 1
+    
+  
     
     @IBOutlet var image1: UIImageView!
     @IBAction func square1(sender: AnyObject) {
@@ -19,13 +55,19 @@ class ViewController: UIViewController {
             
             image1.image = UIImage(named: "X.png")
             
+            xPlays.append(1)
+            
         } else if tapCount == 2 || tapCount == 4 || tapCount == 6 || tapCount == 8 {
             
             image1.image = UIImage(named: "O.png")
             
+            oPlays.append(1)
+            
         }
         
         tapCount += 1
+        
+        winner()
         
     }
     
@@ -36,13 +78,20 @@ class ViewController: UIViewController {
             
             image2.image = UIImage(named: "X.png")
             
+            xPlays.append(2)
+            
         } else if tapCount == 2 || tapCount == 4 || tapCount == 6 || tapCount == 8 {
             
             image2.image = UIImage(named: "O.png")
             
+            oPlays.append(2)
+            
         }
         
         tapCount += 1
+        
+        winner()
+
     }
     
     @IBOutlet var image3: UIImageView!
@@ -52,13 +101,20 @@ class ViewController: UIViewController {
             
             image3.image = UIImage(named: "X.png")
             
+            xPlays.append(3)
+            
         } else if tapCount == 2 || tapCount == 4 || tapCount == 6 || tapCount == 8 {
             
             image3.image = UIImage(named: "O.png")
             
+            oPlays.append(3)
+            
         }
         
         tapCount += 1
+        
+        winner()
+
     }
     
     
@@ -69,13 +125,20 @@ class ViewController: UIViewController {
             
             image4.image = UIImage(named: "X.png")
             
+            xPlays.append(4)
+            
         } else if tapCount == 2 || tapCount == 4 || tapCount == 6 || tapCount == 8 {
             
             image4.image = UIImage(named: "O.png")
             
+            oPlays.append(4)
+            
         }
         
         tapCount += 1
+        
+        winner()
+
     }
     
     @IBOutlet var image5: UIImageView!
@@ -85,13 +148,20 @@ class ViewController: UIViewController {
             
             image5.image = UIImage(named: "X.png")
             
+            xPlays.append(5)
+            
         } else if tapCount == 2 || tapCount == 4 || tapCount == 6 || tapCount == 8 {
             
             image5.image = UIImage(named: "O.png")
             
+            oPlays.append(5)
+            
         }
         
         tapCount += 1
+        
+        winner()
+
     }
     
     @IBOutlet var image6: UIImageView!
@@ -101,13 +171,19 @@ class ViewController: UIViewController {
             
             image6.image = UIImage(named: "X.png")
             
+            xPlays.append(6)
+            
         } else if tapCount == 2 || tapCount == 4 || tapCount == 6 || tapCount == 8 {
             
             image6.image = UIImage(named: "O.png")
             
+            oPlays.append(6)
+            
         }
         
         tapCount += 1
+        
+        winner()
     }
     
     @IBOutlet var image7: UIImageView!
@@ -117,13 +193,20 @@ class ViewController: UIViewController {
             
             image7.image = UIImage(named: "X.png")
             
+            xPlays.append(7)
+            
         } else if tapCount == 2 || tapCount == 4 || tapCount == 6 || tapCount == 8 {
             
             image7.image = UIImage(named: "O.png")
             
+            oPlays.append(7)
+            
         }
         
         tapCount += 1
+        
+        winner()
+
     }
     
     @IBOutlet var image8: UIImageView!
@@ -133,13 +216,20 @@ class ViewController: UIViewController {
             
             image8.image = UIImage(named: "X.png")
             
+            xPlays.append(8)
+            
         } else if tapCount == 2 || tapCount == 4 || tapCount == 6 || tapCount == 8 {
             
             image8.image = UIImage(named: "O.png")
             
+            oPlays.append(8)
+            
         }
         
         tapCount += 1
+        
+        winner()
+
     }
     
     @IBOutlet var image9: UIImageView!
@@ -149,16 +239,34 @@ class ViewController: UIViewController {
             
             image9.image = UIImage(named: "X.png")
             
+            xPlays.append(9)
+            
         } else if tapCount == 2 || tapCount == 4 || tapCount == 6 || tapCount == 8 {
             
             image9.image = UIImage(named: "O.png")
             
+            oPlays.append(9)
+            
         }
         
         tapCount += 1
+        
+        winner()
+        
     }
     
     
+    @IBAction func restartButton(sender: AnyObject) {
+        
+        loadView()
+        
+        xPlays.removeAll()
+        
+        oPlays.removeAll()
+        
+        tapCount = 1
+    
+    }
     
 
     override func viewDidLoad() {
